ba4cb06e74a832f7c4921c2cc255242dadbff906
Max Hellqvist <thexvw@gmail.com>
2021-03-07 03:11:43 +0100

    Add volume setting per target

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,8 @@
 import { db } from "./firebase";
 
-export const registerAnnouncement = (userID: string, audioUrl: string) => {
+export const registerAnnouncement = (userID: string, audioUrl: string, volume: number) => {
     db.child(`registeredTargets/${userID}`).set({ audioUrl });
+    db.child(`registeredTargets/${userID}`).set({ volume });
 }
 
 export const getAnnouncement = async (userID: string) => {
@@ -11,13 +12,17 @@ export const getAnnouncement = async (userID: string) => {
         })
 }
 
+export const setVolume = (userID: string, volume: number) => {
+    db.child(`registeredTargets/${userID}`).update({ volume });
+}
+
 export const setActive = (isActive: boolean) => {
     db.child(`isActive`).set(isActive);
 }
 
 export const getActive = async () => {
     return db.child(`isActive`).get()
-    .then((snap) => {
-        return snap.val();
-    })
+        .then((snap) => {
+            return snap.val();
+        })
 }
